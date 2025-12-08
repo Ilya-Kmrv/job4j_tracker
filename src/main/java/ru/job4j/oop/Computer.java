affaf95ae0a64dfcdda46117b604a8f02ebdc45e
srjm <cyberpiluha@gmail.com>
2025-12-08 18:05:30 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -3,23 +3,44 @@ package ru.job4j.oop;
 public class Computer {
 
     private boolean multiMonitor;
+
     private int ssd;
+
     private String cpu;
 
-    public Computer(boolean multiMonitor, int ssd, String cpu) {
-        this.multiMonitor = multiMonitor;
-        this.ssd = ssd;
-        this.cpu = cpu;
-    }
-
-    public static void main(String[] args) {
-        Computer comp = new Computer(true, 500, "Intel Core I7-10700K");
-        comp.printInfo();
+    public Computer() {
     }
 
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
+    }
+
+    public Computer(boolean multiMonitor, int ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    public static void main(String[] args) {
+        Computer first = new Computer();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        first.printInfo();
+        second.printInfo();
+        third.printInfo();
+        forth.printInfo();
     }
 }
