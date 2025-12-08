5d7bc85c2117c18f0d76dc40d9916f09a2f8aa42
srjm <cyberpiluha@gmail.com>
2025-12-08 17:32:28 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public static void main(String[] args) {
+    Error err = new Error(true, 420, "всё ок");
+    err.printInfo();
+    Error err2 = new Error(false, 404, "Страница не найдена");
+    err2.printInfo();
+    Error def = new Error();
+    def.printInfo();
+}
+    public void printInfo() {
+    System.out.println("Ошибка: " + active);
+    System.out.println("Код: " + status);
+    System.out.println("Подробнее: " + message);
+    }
+}
