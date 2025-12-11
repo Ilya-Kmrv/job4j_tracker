e27231203b86305654ddf8c7616066637e182e12
srjm <cyberpiluha@gmail.com>
2025-12-11 17:35:54 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        LocalDateTime created = time.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(created.format(formatter));
+    }
+}
