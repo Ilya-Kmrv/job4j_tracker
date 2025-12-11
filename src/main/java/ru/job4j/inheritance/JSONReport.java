78c663dd19ca4ce954f22671da52b0543904e138
srjm <cyberpiluha@gmail.com>
2025-12-11 17:02:03 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,8 +1,8 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
-    public String generate(String name, String body) {
+    @Override
+    public String genrate(String name, String body) {
 
         return "{" + System.lineSeparator()
                 + "\t\"name\" : \"" + name + "\"," + System.lineSeparator()
