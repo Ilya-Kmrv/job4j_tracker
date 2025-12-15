7c9188666dd89a9e83470b1fef909e1699a84d29
srjm <cyberpiluha@gmail.com>
2025-12-15 11:16:19 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,4 +22,28 @@ public class Tracker {
         }
         return result;
     }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        for (int index = 0; index < size; index++) {
+            result[index] = items[index];
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] temp = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                temp[count++] = item;
+            }
+        }
+        Item[] result = new Item[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+    }
 }
