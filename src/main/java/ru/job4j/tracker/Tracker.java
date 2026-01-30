06e508db76bbf2f7b2008273ff4bd04b146b6b6f
srjm <cyberpiluha@gmail.com>
2026-01-30 22:29:25 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #556147]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,12 +55,6 @@ public class Tracker {
         }
     }
 
-    public static void main(String[] args) {
-        String[] names = {"Petr", null, "Ivan", "Stepan", "Fedor"};
-        System.arraycopy(names, 2, names, 1, 3);
-        System.out.println(Arrays.toString(names));
-    }
-
     private int indexOf(int id) {
         int result = -1;
         for (int i = 0; i < size; i++) {
