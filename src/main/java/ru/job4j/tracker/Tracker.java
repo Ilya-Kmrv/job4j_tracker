28abf148fca0d876747f1485a314a8bce2cac966
srjm <cyberpiluha@gmail.com>
2026-01-30 12:36:43 +0300

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,18 +53,20 @@ public class Tracker {
         }
     }
 
-    public static void main(String[]args) {
-            String[] names = {"Petr", null, "Ivan", "Stepan", "Fedor"};
-            System.arraycopy(names, 2, names, 1, 3);
-            System.out.println(Arrays.toString(names));
-        }
-
-        private int indexOf(int id) {
-            for (int index = 0; index < size; index++) {
-                if (items[index].getId() == id) {
-                    return index;
-                }
-            }
-            return -1;
-        }
+    public static void main(String[] args) {
+        String[] names = {"Petr", null, "Ivan", "Stepan", "Fedor"};
+        System.arraycopy(names, 2, names, 1, 3);
+        System.out.println(Arrays.toString(names));
     }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
