2a2028c1cd19f253629e3250f413b3c6f44f40d5
srjm <cyberpiluha@gmail.com>
2026-01-30 09:50:43 +0300

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,24 +10,31 @@ public class StartUI {
             showMenu();
             System.out.print("Выбрать: ");
             int select = Integer.parseInt(scanner.nextLine());
-
             if (select == 6) {
                 System.out.println("Завершение работы");
             }
-
             if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
             } else {
                 run = false;
             }
-
+            if (select == 0) {
+                System.out.println("=== Создание новой заявки ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
+                run = false;
+            }
         }
     }
 
     private void showMenu() {
         String[] menu = {"Добавить новую заявку", "Показать все заявки",
-                         "Изменить заявку", "Удалить заявку", "Показать заявку по id",
-                         "Показать заявки по имени", "Завершить программу"};
+                "Изменить заявку", "Удалить заявку", "Показать заявку по id",
+                "Показать заявки по имени", "Завершить программу"};
         System.out.println("Меню:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
