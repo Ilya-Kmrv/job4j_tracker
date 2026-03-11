86f8eed1f49d0626c316dd1c3bd092f743560fd0
srjm <cyberpiluha@gmail.com>
2026-03-11 21:17:32 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.MockInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
@@ -157,7 +163,7 @@ class StartUITest {
     @Test
     void whenFindIdTestOutputIsSuccessfully() {
         Tracker tracker = new Tracker();
-        Item one = tracker.add(new Item("test"));
+        Item one = tracker.add(new Item("test1"));
         Output output = new StubOutput();
         Input input = new MockInput(
                 new String[] {"0", String.valueOf(one.getId()), "1"}
