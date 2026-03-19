ca99a0ebccd0301b032cff35a3c2ec100a1b6f6c
srjm <cyberpiluha@gmail.com>
2026-03-19 10:12:40 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/input/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/input/ValidateInput.java
@@ -1,18 +1,33 @@
 package ru.job4j.tracker.input;
 
-public class ValidateInput extends ConsoleInput {
+import ru.job4j.tracker.output.Output;
+
+public class ValidateInput implements Input {
+    private final Output output;
+    private final Input input;
+
+    public ValidateInput(Output output, Input input) {
+        this.output = output;
+        this.input = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return input.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
-            try {
-                value = super.askInt(question);
-                invalid = false;
-            } catch (NumberFormatException nfe) {
+            String result = input.askStr(question);
+            if (!isNumber(result)) {
                 System.out.println("Пожалуйста, введите корректные данные");
+                continue;
             }
+            value = Integer.parseInt(result);
+            invalid = false;
         } while (invalid);
         return value;
     }
