9dbd9e92667059a958b71552623fafc2bf240a60
srjm <cyberpiluha@gmail.com>
2026-03-25 07:34:28 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -9,19 +9,10 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     *
-     * @param key Ключ поиска.
-     * @return Список пользователей, которые прошли проверку.
-     */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key) ||
-                    person.getPhone().contains(key) ||
-                    person.getAddress().contains(key) ||
-                    person.getSurname().contains(key)) {
+            if (person.getName().contains(key) || person.getPhone().contains(key) || person.getAddress().contains(key) || person.getSurname().contains(key)) {
                 result.add(person);
             }
         }
